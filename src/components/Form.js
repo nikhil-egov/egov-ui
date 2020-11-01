351d3f719ab7d455b9b77f0823d7365d6178afd2
nikhil24 <nikhil15.1990@gmail.com>
2020-11-01 20:50:26 +0530

    remove debugger

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,16 @@
 import React from "react";
 
 const Form = ({ onSubmit, handlesubmit, children, ...props }) => {
-  {
-    //console.log("config::::>>>>>>>>>>>>>>>>>>>>>>1", handlesubmit);
-  }
   const submitForm = async (data) => {
-    debugger;
     const beforeSubmit = props["before-submit"];
     let newData = data;
     if (beforeSubmit && window[beforeSubmit]) {
       newData = await window[beforeSubmit](JSON.parse(JSON.stringify(data)));
     }
-    console.log("form submit", data, newData);
     onSubmit(data, props.redirectto);
   };
   return (
     <form onSubmit={handlesubmit(submitForm)} {...props}>
-      {console.log("config::::>>>>>>>>>>>>>>>>>>>>>>2")}
       {children}
     </form>
   );
